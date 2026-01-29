69d67c736920316c93d6811bfc03610936fa8a5d
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-29 23:39:12 +0200

    added some session storage items and changed some logic

--- homepage.js
+++ homepage.js
@@ -4,13 +4,19 @@ const submittedImageDiv = document.getElementById("submitted-image-div")
 imagePathForm.addEventListener("submit", async function submitimage(e) {
 	e.preventDefault();
 	const imagePath = document.getElementById("image-path").value;
+	sessionStorage.set('image_path', imagePath)
 	const response = await fetch(`http://127.0.0.1:8000/uploadimage/${imagePath}`);
 
 	const imageBlob = await response.blob();
 	const imageUrl = URL.createObjectURL(imageBlob);
+	sessionStorage.set('image_url', imageUrl)
 	const img = document.createElement("img");
 	img.src = imageUrl;
 
 	submittedImageDiv.innerHTML = "";
 	submittedImageDiv.appendChild(img);
 })
+
+const directToCropButton = document.getElementById("direct-to-crop-button")
+
+directToCropButton.addEventListener("click", window.location.assign("http://localhost:5500/crop.html"))
