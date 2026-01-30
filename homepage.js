ead705747f57194b4247a02c601f6ccb35d10f19
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-30 22:24:21 +0200

    slight but substancial changes

--- homepage.js
+++ homepage.js
@@ -1,15 +1,16 @@
 const imagePathForm = document.getElementById("image-path-form")
 const submittedImageDiv = document.getElementById("submitted-image-div")
 
+
 imagePathForm.addEventListener("submit", async function submitimage(e) {
 	e.preventDefault();
 	const imagePath = document.getElementById("image-path").value;
-	sessionStorage.set('image_path', imagePath)
+	sessionStorage.setItem('image_path', imagePath)
 	const response = await fetch(`http://127.0.0.1:8000/uploadimage/${imagePath}`);
 
 	const imageBlob = await response.blob();
 	const imageUrl = URL.createObjectURL(imageBlob);
-	sessionStorage.set('image_url', imageUrl)
+	sessionStorage.setItem('image_url', imageUrl);
 	const img = document.createElement("img");
 	img.src = imageUrl;
 
@@ -17,6 +18,10 @@ imagePathForm.addEventListener("submit", async function submitimage(e) {
 	submittedImageDiv.appendChild(img);
 })
 
+
 const directToCropButton = document.getElementById("direct-to-crop-button")
 
-directToCropButton.addEventListener("click", window.location.assign("http://localhost:5500/crop.html"))
+
+directToCropButton.addEventListener("click", function directToCropPage() {
+	window.location.assign("http://localhost:5500/crop.html")
+})
