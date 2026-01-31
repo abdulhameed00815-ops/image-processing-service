72da3fbc2902033bcedeb564b1fb392c63fb084c
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-31 23:28:19 +0200

    attempted to add a function that checks if the image is stored in sessionStorage

--- homepage.js
+++ homepage.js
@@ -1,22 +1,37 @@
 const imagePathForm = document.getElementById("image-path-form")
 const submittedImageDiv = document.getElementById("submitted-image-div")
 
+function checkIfImageExists() {
+	const imageUrl = sessionStorage.getItem("image_url")
+	if (imageUrl !== null) {
+		const imageUrl = sessionStorage.getItem("image_url");
+		const img = document.createElement("img");
+		img.src = imageUrl;
 
-imagePathForm.addEventListener("submit", async function submitimage(e) {
-	e.preventDefault();
-	const imagePath = document.getElementById("image-path").value;
-	sessionStorage.setItem('image_path', imagePath)
-	const response = await fetch(`http://127.0.0.1:8000/uploadimage/${imagePath}`);
+		submittedImageDiv.innerHTML = "";
+		submittedImageDiv.appendChild(img);
+	} else {
+		imagePathForm.addEventListener("submit", async function submitimage(e) {
+			e.preventDefault();
+			const imagePath = document.getElementById("image-path").value;
+			sessionStorage.setItem('image_path', imagePath)
+			const response = await fetch(`http://127.0.0.1:8000/uploadimage/${imagePath}`);
 
-	const imageBlob = await response.blob();
-	const imageUrl = URL.createObjectURL(imageBlob);
-	sessionStorage.setItem('image_url', imageUrl);
-	const img = document.createElement("img");
-	img.src = imageUrl;
+			const imageBlob = await response.blob();
+			const imageUrl = URL.createObjectURL(imageBlob);
+			sessionStorage.setItem('image_url', imageUrl);
+			const img = document.createElement("img");
+			img.src = imageUrl;
 
-	submittedImageDiv.innerHTML = "";
-	submittedImageDiv.appendChild(img);
-})
+			submittedImageDiv.innerHTML = "";
+			submittedImageDiv.appendChild(img);
+		})
+	}
+}
+
+window.onload = checkIfImageExists();
+
+
 
 
 const directToCropButton = document.getElementById("direct-to-crop-button")
