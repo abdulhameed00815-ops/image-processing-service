aa67fe71d9fa8e01b71aac65bf15c654ce169e2f
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-01-31 23:28:52 +0200

    made changes to the crop endpoint

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
-from io import BytesIO
+import io
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from PIL import Image
+from fastapi.responses import Response, FileResponse
+from PIL import Image, ImageOps
 import os
 import httpx
 
@@ -42,13 +42,13 @@ async def operate_image(image_path: str, operation: str, request: Request):
 #this is just a dummy endpoint, i still need to figure out how to make a copy of the original image after cropping it, also need to rename it, but the main logic is ok.
 @fastapi.post("/crop/{crop_amount1}/{crop_amount2}/{crop_amount3}/{crop_amount4}")
 async def crop_image(crop_amount1: int, crop_amount2: int, crop_amount3: int, crop_amount4: int, image: UploadFile = File(...)):
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        image_data = BytesIO(response.content)
-        im = Image.open(image_data)
-        box = (crop_amount1, crop_amount2, crop_amount3, crop_amount4)
-        cropped_image = im.paste(region, box)
-        return FileResponse(cropped_image, media_type="image/jpeg")
+    im = Image.open(image.file)
+    cropped_image = ImageOps.crop(im, border=(crop_amount1, crop_amount2, crop_amount3, crop_amount4))
+    #we use the code below to temporarily store the file in the ram (so that we can return it thro http) instead of storing it on the disc.
+    buf = io.BytesIO()
+    cropped_image.save(buf, format="JPEG")
+    buf.seek(0)
+    return Response(content=buf.read(), media_type="image/jpeg")
 
 
 @fastapi.get("/rotate/{image_url:path}")
