4838582ca9373a409c9a3151cf2292cb7b88d3dd
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2026-02-02 21:27:33 +0200

    made the crop page more graceful to redoing the operation on the image

--- crop.js
+++ crop.js
@@ -1,9 +1,9 @@
 const submittedImageDiv = document.getElementById("submitted-image-div")
-const imageUrl = sessionStorage.getItem('image_url');
 
 
 window.onload = function loadUploadedImage() {
-        const img = document.createElement("img");
+	let imageUrl = sessionStorage.getItem("image_url");
+        let img = document.createElement("img");
         img.src = imageUrl;
 
         submittedImageDiv.innerHTML = "";
@@ -13,10 +13,10 @@ window.onload = function loadUploadedImage() {
 
 async function blobUrlToFile(blobUrl, fileName) {
 	try {
-	const response = await fetch(blobUrl);
-	const blob = await response.blob();
+	let response = await fetch(blobUrl);
+	let blob = await response.blob();
 
-	const file = new File([blob], fileName, { type: blob.type, lastModified: Date.now() });
+	let file = new File([blob], fileName, { type: blob.type, lastModified: Date.now() });
 	return file;
 	} catch (error) {
 	console.error("Error converting blob URL to File:", error);
@@ -26,7 +26,7 @@ async function blobUrlToFile(blobUrl, fileName) {
 
 
 function downloadImageFromBlob(blobUrl, filename) {
-	const link = document.createElement('a');
+	let link = document.createElement('a');
 
 	link.href = blobUrl;
 
@@ -47,35 +47,45 @@ const cropAmountForm = document.getElementById("crop-amount-form");
 
 cropAmountForm.addEventListener("submit", async function cropImage (e) {
 	e.preventDefault()
-	const file = await blobUrlToFile(imageUrl, "image.jpg");
+	let imageUrl = sessionStorage.getItem("image_url");
+	let file = await blobUrlToFile(imageUrl, "image.jpg");
 	//we create a form data to let the browser send the file safely thro http
-	const formData = new FormData();
+	let formData = new FormData();
 	formData.append("image", file)
-	const cropAmount1 = document.getElementById("crop-amount-1").value;
-	const cropAmount2 = document.getElementById("crop-amount-2").value;
-	const cropAmount3 = document.getElementById("crop-amount-3").value;
-	const cropAmount4 = document.getElementById("crop-amount-4").value;
-        const response = await fetch(`http://127.0.0.1:8000/crop/${cropAmount1}/${cropAmount2}/${cropAmount3}/${cropAmount4}`, {
+	let cropAmount1 = document.getElementById("crop-amount-1").value;
+	let cropAmount2 = document.getElementById("crop-amount-2").value;
+	let cropAmount3 = document.getElementById("crop-amount-3").value;
+	let cropAmount4 = document.getElementById("crop-amount-4").value;
+	let values = [cropAmount1, cropAmount2, cropAmount3, cropAmount4];
+	let actualValues = values.map(v => v === "" ? "0" : v);
+        let response = await fetch(`http://127.0.0.1:8000/crop/${actualValues[0]}/${actualValues[1]}/${actualValues[2]}/${actualValues[3]}`, {
 		method: "POST",
 		body: formData
 	});
 
-        const imageBlob = await response.blob();
-        const croppedImageUrl = URL.createObjectURL(imageBlob);
+        let imageBlob = await response.blob();
+        let croppedImageUrl = URL.createObjectURL(imageBlob);
         sessionStorage.setItem('image_url', croppedImageUrl);
-        const img = document.createElement("img");
+        let img = document.createElement("img");
         img.src = croppedImageUrl;
 
         submittedImageDiv.innerHTML = "";
         submittedImageDiv.appendChild(img);
+	for (const key of formData.keys()) {
+		formData.delete(key);
+	}
 	
-	const downloadButton = document.getElementById("download-button");
-	downloadButton.addEventListener("click", function() {
-		downloadImageFromBlob(croppedImageUrl, "cropped_image.jpg");
-	})
+})
 
-	const directToHomepage = document.getElementById("direct-to-homepage");
-	directToHomepage.addEventListener("click", function() {
-		window.location.assign("http://localhost:5500/homepage.html");
-	})
+
+const downloadButton = document.getElementById("download-button");
+downloadButton.addEventListener("click", function() {
+	let imageUrl = sessionStorage.getItem("image_url");
+	downloadImageFromBlob(imageUrl, "image.jpg");
+})
+
+
+const directToHomepage = document.getElementById("direct-to-homepage");
+directToHomepage.addEventListener("click", function() {
+	window.location.assign("http://localhost:5500/homepage.html");
 })
